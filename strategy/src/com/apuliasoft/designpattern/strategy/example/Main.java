182743d71b7c599678522c98105dad49c8c45318
Nicola Sanitate <n.sanitate@apuliasoft.com>
2018-03-16 18:31:51 +0100

    style(strategy): fixed indentation in main

--- strategy/src/com/apuliasoft/designpattern/strategy/example/Main.java
+++ strategy/src/com/apuliasoft/designpattern/strategy/example/Main.java
@@ -2,14 +2,14 @@ package com.apuliasoft.designpattern.strategy.example;
 
 public class Main {
     public static void main(String[] args) {
-      // Run behaviour A
-      IFlyBehaviour simpleFlyBehaviour = new SimpleFlyBehaviour();
-      Duck simpleFlyingDuck = new Duck(simpleFlyBehaviour);
-      simpleFlyingDuck.fly();
-      
-      // Run behaviour B
-      IFlyBehaviour noFlyBehaviour = new NoFlyBehaviour();
-      Duck noFlyingDuck = new Duck(noFlyBehaviour);
-      noFlyingDuck.fly();
+        // Run behaviour A
+        IFlyBehaviour simpleFlyBehaviour = new SimpleFlyBehaviour();
+        Duck simpleFlyingDuck = new Duck(simpleFlyBehaviour);
+        simpleFlyingDuck.fly();
+
+        // Run behaviour B
+        IFlyBehaviour noFlyBehaviour = new NoFlyBehaviour();
+        Duck noFlyingDuck = new Duck(noFlyBehaviour);
+        noFlyingDuck.fly();
     }
-  }
+}
